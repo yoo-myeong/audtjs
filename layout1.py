e1a1728d2dac137b5ebd28632f2bd9b87db11517
zlapvmf <61347731+zlapvmf@users.noreply.github.com>
2020-03-06 13:36:05 +0900

    Create layout1.py

---
+++ layout1.py
@@ -0,0 +1,112 @@
+from tkinter import *
+from tkinter import messagebox
+window = Tk()
+window.geometry("800x480")
+
+number = 1234
+
+display = Entry(window, width= 15, justify ="right", show = '*') # justify = "right" 숫자가 오른쪽으로부터
+def button_press(val): #입력받기
+    display.insert("end",val) #display 입력창의 맨끝에 매개변수값을 입력
+
+def b_correct():
+    val = Entry.get(display)
+    answer =1234
+    number = val
+    if int(number) == answer:
+        return messagebox.showinfo("확인", "정답입니다.")
+    else:
+        display.delete(0,END)
+        return messagebox.showinfo("확인","틀렸습니다.")
+
+def clear():
+    mylist = window.place_slaves()
+    for i in mylist:
+        i.destroy()
+
+def doorlock():
+    mylist = window.place_slaves()
+    for i in mylist:
+        i.destroy()
+
+        button_7 = Button(window, text='7', height=2, width=10, command=lambda: button_press('7'))
+        button_8 = Button(window, text='8', height=2, width=10, command=lambda: button_press('8'))
+        button_9 = Button(window, text='9', height=2, width=10, command=lambda: button_press('9'))
+
+        button_4 = Button(window, text='4', height=2, width=10, command=lambda: button_press('4'))
+        button_5 = Button(window, text='5', height=2, width=10, command=lambda: button_press('5'))
+        button_6 = Button(window, text='6', height=2, width=10, command=lambda: button_press('6'))
+
+        button_1 = Button(window, text='1', height=2, width=10, command=lambda: button_press('1'))
+        button_2 = Button(window, text='2', height=2, width=10, command=lambda: button_press('2'))
+        button_3 = Button(window, text='3', height=2, width=10, command=lambda: button_press('3'))
+
+        button_0 = Button(window, text='0', height=2, width=10, command=lambda: button_press('0'))
+        button_dat = Button(window, text='*', height=2, width=10, command=lambda: cal('*'))
+        button_equal = Button(window, text='#', height=2, width=10, command=lambda: cal('#'))
+
+        button_7.place(x=600, y=140)
+        button_8.place(x=690, y=140)
+        button_9.place(x=780, y=140)
+
+        button_4.place(x=600, y=240)
+        button_5.place(x=690, y=240)
+        button_6.place(x=780, y=240)
+
+        button_1.place(x=600, y=340)
+        button_2.place(x=690, y=340)
+        button_3.place(x=780, y=340)
+
+        button_dat.place(x=600, y=440)
+        button_0.place(x=690, y=440)
+        button_equal.place(x=780, y=440)
+
+        Button(window, text='뒤로가기', width=20, command=lambda:cal2('화면을 터치해주세요')).place(x=650, y=100)
+
+        label = Entry(window, width=24, justify='right')
+        label.place(x=640, y=50)
+
+    def cal(key):
+        if key == "#":
+            result = str(label.get())
+            label.delete(0, END)
+            if result == number:
+                label.insert(END, "문이 열립니다")
+            else:
+                label.insert(END, "*를 누르고 다시 입력하시오")
+        elif key == "*":
+            label.delete(0, END)
+
+    def button_press(val):
+        label.insert(END, val)
+
+
+
+
+
+def cal2(key):
+    if key=="화면을 터치해주세요":
+        mylist = window.place_slaves()
+        for i in mylist:
+            i.destroy()
+
+            window.configure(bg="blue")
+
+            call=PhotoImage(file="md_b.gif")
+            w_lb= Button(window,height=100,width=100,image=call)
+            w_lb.place(x=600,y=100)
+
+            pass2 = Button(window,text='pass',width=5,command=doorlock)
+            pass2.place(x=600, y=300)
+
+
+
+
+
+button1 = Button(window, text='화면을 터치해주세요', command=lambda: cal2('화면을 터치해주세요')).place(x=330,y=220)
+
+Label(window, text ="xx동 xx호").place(x=360,y=150)
+
+
+
+window.mainloop()
